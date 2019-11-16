27c4baf423b39011909ffcf5e4507ff522f609c1
domotord90 <jazzmaster@freemail.com>
2019-11-16 19:24:05 +0100

    Updates

---
+++ precache-manifest.27fe28bc60650ce705f78b5aa73117b1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "674d37dd82d4db7f4146847f4ca31232",
+    "url": "/react-youtube-search/index.html"
+  },
+  {
+    "revision": "50d942f16661f0e978c5",
+    "url": "/react-youtube-search/static/js/2.42ee7370.chunk.js"
+  },
+  {
+    "revision": "521d5f5f6ac7c89456c4",
+    "url": "/react-youtube-search/static/js/main.55aa306f.chunk.js"
+  },
+  {
+    "revision": "6ee5bad95b0f64b5c572",
+    "url": "/react-youtube-search/static/js/runtime~main.c50d4f57.js"
+  }
+]);
